64734e5792890086a732f9a029deaca935ecaf42
ctorgerson10 <ctorgerson10@gmail.com>
2023-04-13 17:40:36 -0600

    added zoom, mapping WIP

--- FractalDrawer.java
+++ FractalDrawer.java
@@ -1,27 +1,39 @@
+import java.awt.image.BufferedImage;
+
 public class FractalDrawer {
 
-    public FractalDrawer() {
-        initializeColorMap();
-    }
+    // Color variables
+    public int[] colors;
+    public static final int black = 0;
 
-    //Color variables
-    public int[] colors = new int[16];
-    public static int black = 0;
+    // Image output variables
+    public BufferedImage image;
+    public int[] pixelColors; // 1d array representing all the pixels in the image
+    public int numberOfFrames;
+
+    // constructor
+    public FractalDrawer(int width, int height, int numberOfFrames) {
+        this.colors = new int[16];
+        this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        this.pixelColors = new int[width * height];
+        this.numberOfFrames = numberOfFrames;
+        initializeColorMap();
+        for (int i = 0; i < pixelColors.length; i++) {
+            pixelColors[i] = 0xFF000000; // set all pixels to black with 100% alpha to start
+        }
+        // set pixels on image
+        this.image.setRGB(0, 0, width, height, pixelColors, 0, width);
+    }
 
     /**
      * convert RGB value to integer for use by the BufferedImage class
-     * 
-     * @param red
-     * @param green
-     * @param blue
-     * @return
+     * code inspired by CodeSpaceIndica on Stackoverflow
      */
-    public static int getIntFromColor(int red, int green, int blue) {
-        red   = (red << 16) & 0xFF0000;   //Shift red 16-bits
-        green = (green << 8) & 0x00FF00;  //Shift Green 8-bits
-        blue  = blue & 0x0000FF;          
-
-        return 0x000000 | red | green | blue; //0x000000 Bitwise OR to combine the colors
+    public static int RGBtoInt(int r, int g, int b) {
+        r = (r << 16) & 0x00FF0000;    // bitwise AND masks all values but red, << shift red 16 bits
+        g = (g << 8) & 0x0000FF00;     // bitwise AND masks all values but green, << shift green 8 bits
+        b = b & 0x000000FF;            // bitwise AND masks all values but blue
+        return 0xFF000000 | r | g | b; // bitwise OR to combine everything and return integer value.
     }
 
     /**
@@ -45,24 +57,22 @@ public class FractalDrawer {
      * 
      */
     private void initializeColorMap() {
-        this.colors[0]  = getIntFromColor(66, 30, 15);
-        this.colors[1]  = getIntFromColor(25, 7, 26);
-        this.colors[2]  = getIntFromColor(9, 1, 47);
-        this.colors[3]  = getIntFromColor(4, 4, 73);
-        this.colors[4]  = getIntFromColor(0, 7, 100);
-        this.colors[5]  = getIntFromColor(12, 44, 138);
-        this.colors[6]  = getIntFromColor(24, 82, 177);
-        this.colors[7]  = getIntFromColor(57, 125, 209);
-        this.colors[8]  = getIntFromColor(134, 181, 229);
-        this.colors[9]  = getIntFromColor(211, 236, 248);
-        this.colors[10] = getIntFromColor(241, 233, 191);
-        this.colors[11] = getIntFromColor(248, 201, 95);
-        this.colors[12] = getIntFromColor(255, 170, 0);
-        this.colors[13] = getIntFromColor(204, 128, 0);
-        this.colors[14] = getIntFromColor(153, 87, 0);
-        this.colors[15] = getIntFromColor(106, 52, 3);
-
-        black = getIntFromColor(0, 0, 0);
+        this.colors[0]  = RGBtoInt(66, 30, 15);
+        this.colors[1]  = RGBtoInt(25, 7, 26);
+        this.colors[2]  = RGBtoInt(9, 1, 47);
+        this.colors[3]  = RGBtoInt(4, 4, 73);
+        this.colors[4]  = RGBtoInt(0, 7, 100);
+        this.colors[5]  = RGBtoInt(12, 44, 138);
+        this.colors[6]  = RGBtoInt(24, 82, 177);
+        this.colors[7]  = RGBtoInt(57, 125, 209);
+        this.colors[8]  = RGBtoInt(134, 181, 229);
+        this.colors[9]  = RGBtoInt(211, 236, 248);
+        this.colors[10] = RGBtoInt(241, 233, 191);
+        this.colors[11] = RGBtoInt(248, 201, 95);
+        this.colors[12] = RGBtoInt(255, 170, 0);
+        this.colors[13] = RGBtoInt(204, 128, 0);
+        this.colors[14] = RGBtoInt(153, 87, 0);
+        this.colors[15] = RGBtoInt(106, 52, 3);
     }
 
     
--- Mandelbrot.java
+++ Mandelbrot.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 /**
  * Implementation of the mandelbrot set using the ComplexNum class.
@@ -10,28 +13,37 @@ public class Mandelbrot {
 	// width and height of the window
 	static final int WIDTH = 1000;
 	static final int HEIGHT = 1000;
-	static final int NUM_FRAMES = 100;
+	static final int NUM_FRAMES = 10;
 
 	// output image variables
-	static BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-	static int[] pixelColors = new int[WIDTH * HEIGHT];
-	static FractalDrawer fd = new FractalDrawer();
+	static FractalDrawer fd = new FractalDrawer(WIDTH, HEIGHT, NUM_FRAMES);
 
-	// starting coordinates
+	// starting MIN and MAX coordinate values -- [-2, 2] encapsulates the full mandelbrot set
 	static BigDecimal STARTING_MIN_X = new BigDecimal(-2);
 	static BigDecimal STARTING_MAX_X = new BigDecimal(2);
 	static BigDecimal STARTING_MIN_Y = new BigDecimal(-2);
 	static BigDecimal STARTING_MAX_Y = new BigDecimal(2);
 
-	// ending coordinates
-	// blah blah blah
-	
+	// ending MIN and MAX coordinate values -- I took these values from a stackoverflow thread
+	// of interesting zoom locations on the mandelbrot set.
+	static BigDecimal ENDING_MIN_X = new BigDecimal(-1.2576470439078538);
+	static BigDecimal ENDING_MIN_Y = new BigDecimal(0.3780652779236957);
+	static BigDecimal ENDING_MAX_X = new BigDecimal(-1.2576470439074896);
+	static BigDecimal ENDING_MAX_Y = new BigDecimal(0.3780652779240597);
+
+	// increment values -- i.e. how fast we are zooming in
+	// this scales with how many frames we want to generate
+	static BigDecimal MIN_X_INC = ENDING_MIN_X.subtract(STARTING_MIN_X).divide(new BigDecimal(NUM_FRAMES));
+	static BigDecimal MIN_Y_INC = ENDING_MIN_Y.subtract(STARTING_MIN_Y).divide(new BigDecimal(NUM_FRAMES));
+	static BigDecimal MAX_X_INC = ENDING_MAX_X.subtract(STARTING_MAX_X).divide(new BigDecimal(NUM_FRAMES));
+	static BigDecimal MAX_Y_INC = ENDING_MAX_Y.subtract(STARTING_MAX_Y).divide(new BigDecimal(NUM_FRAMES));
+
 	/**
 	 * Default setting for the max iterations the algorithm will test 
 	 * before calling it inside the set or not.
-	 * TODO: Have this scale with screen size for accurate rendering.
 	 */
-	final static int MAX_ITERATIONS = 80;
+	final static int MAX_ITERATIONS = 1000;
+	final static int THRESHOLD = 10;
 	
 	/**
 	 * Tests the divergence of an input complex number c in the 
@@ -70,16 +82,69 @@ public class Mandelbrot {
 		
 		return i;
 	}
-	
 
-	// TODO: change for loop bounds to WIDTH and HEIGHT and uncomment color picking code
-	// TODO: incrementally decrease bounds (increase zoom), and write output to file.
+	public static BigDecimal mapToCoordinatePlane(int input, BigDecimal minOutput, BigDecimal maxOutput) {
+
+		BigDecimal i = new BigDecimal(input);
+		BigDecimal width = new BigDecimal(WIDTH);
+
+		return i.multiply(maxOutput.subtract(minOutput)).divide(width.add(minOutput), ComplexNum.DEFAULT_ROUNDING);
+	}
+
+	/**
+	 * Calculate mandelbrot set and set image pixels for output
+	 */
+	public static void drawFrames() {
+		int frame_iter = 0;
+		while (
+			STARTING_MIN_X.compareTo(ENDING_MIN_X) == -1 ||
+			STARTING_MIN_Y.compareTo(ENDING_MIN_Y) == -1 ||
+			STARTING_MAX_X.compareTo(ENDING_MAX_X) == 1  ||
+			STARTING_MAX_Y.compareTo(ENDING_MAX_Y) == 1  ){
+				
+				for (int x = 0; x < WIDTH; x++) {
+					for (int y = 0; y < HEIGHT; y++) {
+						// map pixel x/y values to values inside [-2, 2]
+						BigDecimal real = mapToCoordinatePlane(x, STARTING_MIN_X, STARTING_MAX_X);
+						BigDecimal imaginary = mapToCoordinatePlane(y, STARTING_MIN_Y, STARTING_MAX_Y);
+						ComplexNum c = new ComplexNum(real, imaginary);
+						int n = testDivergence(c);
+						int color = fd.getColor(n, MAX_ITERATIONS);
+						fd.pixelColors[y + x * WIDTH] = color;
+					}
+				}
+
+				// map color values to image pixels
+				fd.image.setRGB(0, 0, WIDTH, HEIGHT, fd.pixelColors, 0, WIDTH);
+
+				// save image file
+				String fileName = "images/frame" + frame_iter + ".png";
+				File f = new File(fileName);
+				try {
+					ImageIO.write(fd.image, "png", f);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Saved frame " + frame_iter + " as " + fileName);
+
+				// increment bounds
+				STARTING_MIN_X = STARTING_MIN_X.add(MIN_X_INC);
+				STARTING_MIN_Y = STARTING_MIN_Y.add(MIN_Y_INC);
+				STARTING_MAX_X = STARTING_MAX_X.add(MAX_X_INC);
+				STARTING_MAX_Y = STARTING_MAX_Y.add(MAX_Y_INC);
+
+				frame_iter++;
+			}
+	}
+
 	/**
 	 * Prints to console are rough approximation of the Mandelbrot set 
 	 * using "*" for being in the set, and " " for being outside it.
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		// I kept this code because I think it's cute
 		for (double y = 1; y >= -1 ; y-=0.05) {
 			for (double x = -2; x <= 0.5; x+=0.025) {
 				ComplexNum c = new ComplexNum(x, y);
@@ -94,7 +159,9 @@ public class Mandelbrot {
 			}
 			System.out.println();
 		}
-		
+
+		// the real deal
+		drawFrames();
 	}
 
 }
